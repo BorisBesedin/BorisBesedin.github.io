48ad7c3aeba4311087f285ff1f1233428913e514
BorisBesedin <44293650+BorisBesedin@users.noreply.github.com>
2020-01-14 17:38:14 +0700

    блок с контактами, прокрутка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 var experienceBtn = document.querySelector('.certificate_btn');
 var experienceList = document.querySelector('.certificate_list');
-console.log('[eq');
+
+var portfolioButton = document.querySelector('.portfolio_button');
+
+var portfolioSection = document.querySelector('.portfolio');
+
+var scroll = function (element) {
+	var coord = element.offsetTop;
+	window.scrollTo({
+	    top: coord,
+	    behavior: "smooth"
+	});
+};
 
 experienceBtn.addEventListener('click', function () {
 	console.log('[eq');
@@ -14,4 +25,8 @@ experienceBtn.addEventListener('click', function () {
 		experienceBtn.textContent = 'Показать сертификаты';
 		experienceBtn.classList.remove('certificate_btn--active');
 	}
+});
+
+portfolioButton.addEventListener('click', function () {
+	scroll(portfolioSection);
 });
