79b1d8e4bbe8f187298dbad5b8806cd342159e66
BorisBesedin <44293650+BorisBesedin@users.noreply.github.com>
2020-03-05 22:18:58 +0700

    jp test правки

---
+++ projects/jp-test/js/popup.js
@@ -0,0 +1,15 @@
+(function() {
+    let popup = document.querySelector('.search-popup'),
+        searchBtn = document.querySelector('#search-btn'),
+        closeBtn = document.querySelector('.search-popup__header-back');
+
+    searchBtn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        popup.classList.add('search-popup--show');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        popup.classList.remove('search-popup--show');
+    });
+}());
+//# sourceMappingURL=popup.js.map
